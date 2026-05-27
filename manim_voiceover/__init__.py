358f355d21062b0efffa5214e20fa67187b33068
syhdev <syh.dev@gmail.com>
2026-05-27 15:46:46 +0200

    Some adjustments

--- manim_voiceover/__init__.py
+++ manim_voiceover/__init__.py
@@ -1,6 +1,10 @@
 from manim_voiceover.tracker import VoiceoverTracker
 from manim_voiceover.voiceover_scene import VoiceoverScene
 
-import pkg_resources
+# import pkg_resources
 
-__version__: str = pkg_resources.get_distribution(__name__).version
+import importlib.metadata
+
+# __version__: str = pkg_resources.get_distribution(__name__).version
+
+__version__: str = importlib.metadata.version(__name__)
--- tests/test_install.py
+++ tests/test_install.py
@@ -1,2 +1,5 @@
 def test_import():
     from manim_voiceover import VoiceoverScene
+    print(VoiceoverScene)
+
+test_import()
